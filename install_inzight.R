4e24288bf64ba6bed29697144f7affa5a0662f22
Tom Elliott <tomelliottnz@gmail.com>
2019-06-02 14:17:37 +1200

    Create install_inzight.R

---
+++ install_inzight.R
@@ -0,0 +1,21 @@
+options(
+  repos = c(
+    inzight = 'https://r.docker.stat.auckland.ac.nz',
+    CRAN = 'https://cran.rstudio.com'
+  )
+)
+
+install.packages(
+  c(
+    'iNZight',
+    'iNZightModules',
+    'iNZightPlots',
+    'iNZightMR',
+    'iNZightTS',
+    'iNZightTools',
+    'iNZightRegression',
+    'vit',
+    'FutureLearnData'
+   ),
+   dependencies = TRUE
+)
