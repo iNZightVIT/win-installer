32b78f150e2c40ff525f4f4840de63038f0dc8f0
Tom Elliott <tomelliottnz@gmail.com>
2019-07-22 10:27:21 +1200

    Move update script to launcher

--- update.R
+++
@@ -1,8 +0,0 @@
-repo <- "https://r.docker.stat.auckland.ac.nz"
-
-# update the updater if any updates are available
-cat("* Checking if the updater needs updating ...\n")
-utils::update.packages("iNZightUpdate", repos = repo)
-
-# then run the updater
-iNZightUpdate::update("windows")
