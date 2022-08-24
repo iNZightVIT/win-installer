4fb01e613e1383a98fef0f6ff9cb74cffff0793f
Tom Elliott <tomelliottnz@gmail.com>
2022-08-24 13:46:05 +1200

    set default option names

--- launcher.R
+++ launcher.R
@@ -3,20 +3,20 @@ setwd('..')
 
 
 options(
-	help_type = 'html',
-	inzight.disable.bootstraps = TRUE,
-	inzight.lock.packages = TRUE,
-	inzight.default.dev.features = TRUE,
-	inzight.default.multiple_x = TRUE
+    help_type = 'html',
+    inzight.disable.bootstraps = TRUE,
+    inzight.lock.packages = TRUE,
+    inzight.default.dev.features = TRUE,
+    inzight.default.multiple_x = TRUE
 )
 
 Sys.setenv(
-	'R_HOME' = file.path(getwd(), 'R'),
-	'R_USER_CONFIG_DIR' = file.path(getwd(), ".config"),
-	'R_USER_CACHE_DIR' = file.path(getwd(), ".cache"),
-	'R_USER_DATA_DIR' = file.path(getwd(), "data"),
-	'INZIGHT_MODULES_DIR' = file.path(getwd(), "modules"),
-	'R_CACHE_ROOTPATH' = file.path(getwd(), ".cache")
+    'R_HOME' = file.path(getwd(), 'R'),
+    'R_USER_CONFIG_DIR' = file.path(getwd(), ".config"),
+    'R_USER_CACHE_DIR' = file.path(getwd(), ".cache"),
+    'R_USER_DATA_DIR' = file.path(getwd(), "data"),
+    'INZIGHT_MODULES_DIR' = file.path(getwd(), "modules"),
+    'R_CACHE_ROOTPATH' = file.path(getwd(), ".cache")
 )
 
 #### Notes
@@ -60,94 +60,97 @@ ggplot2::update_geom_defaults("bar", list(fill = guinz_palette$primary[2]))
 ggplot2::theme_set(theme_guinz())
 
 ## set default colour palettes for iNZight and ggplot2
+cat_pal <- c(guinz_palette$primary[1:2], guinz_palette$secondary, guinz_palette$primary[3])
 options(
-    inzight.default.palette.cat = c(guinz_palette$primary, guinz_palette$secondary),
-	inzight.default.par = iNZightPlots::inzpar(
-		bar.fill = guinz_palette$primary[2]
-	),
-    ggplot2.discrete.fill = c(guinz_palette$primary, guinz_palette$secondary)
+    inzight.default.palette.cat = cat_pal,
+    inzight.default.par = iNZightPlots::inzpar(
+        bar.fill = guinz_palette$primary[2]
+    ),
+    inzight.default.ggtheme.name = "GUiNZ (Default)",
+    inzight.default.palette.cat.name = "GUiNZ",
+    ggplot2.discrete.fill = cat_pal
 )
 
 start_app <- function(app = c('inzight', 'vit', 'update')) {
-	app = match.arg(app)
+    app = match.arg(app)
 
-	if (app == 'update') {
-		do_update()
-		return()
-	}
+    if (app == 'update') {
+        do_update()
+        return()
+    }
 
-	# display loading screen
-	grDevices::dev.new(width = 5, height = 2)
-	grid::grid.newpage()
-	# Will try to draw a raster if possible, otherwise an array of pixels
+    # display loading screen
+    grDevices::dev.new(width = 5, height = 2)
+    grid::grid.newpage()
+    # Will try to draw a raster if possible, otherwise an array of pixels
 
-	try({
-		suppressWarnings({
-			splashImg <- png::readPNG(
-				file.path(
-					getwd(),
-					"inst",
-					"inzight-banner_guinz.png"
-				),
-			    exists("rasterImage")
-			)
-		})
-		grid::grid.raster(splashImg)
-	}, silent = TRUE)
+    try({
+        suppressWarnings({
+            splashImg <- png::readPNG(
+                file.path(
+                    getwd(),
+                    "inst",
+                    "inzight-banner_guinz.png"
+                ),
+                exists("rasterImage")
+            )
+        })
+        grid::grid.raster(splashImg)
+    }, silent = TRUE)
 
-	message("(Dept of Statistics, Uni. of Auckland)")
-	message("")
-	message(
-		sprintf("Please wait while %s loads...",
-			switch(app, 'inzight' = 'iNZight', 'vit' = 'VIT')
-		)
-	)
+    message("(Dept of Statistics, Uni. of Auckland)")
+    message("")
+    message(
+        sprintf("Please wait while %s loads...",
+            switch(app, 'inzight' = 'iNZight', 'vit' = 'VIT')
+        )
+    )
 
-	# load iNZight/VIT
-	suppressMessages(
-		suppressWarnings({
-			switch(app,
-	  			'inzight' = library(iNZight),
-	  			'vit' = library(vit)
-			)
-		})
-	)
+    # load iNZight/VIT
+    suppressMessages(
+        suppressWarnings({
+            switch(app,
+                  'inzight' = library(iNZight),
+                  'vit' = library(vit)
+            )
+        })
+    )
 
-	# Killing the splash screen, assigning to remove print
-	tmp <- grDevices::dev.off()
-	rm(tmp)
+    # Killing the splash screen, assigning to remove print
+    tmp <- grDevices::dev.off()
+    rm(tmp)
 
-	awin <- NULL
-	if (app == "inzight" &&
-		utils::packageVersion("iNZight") > numeric_version('4.1.4')) {
+    awin <- NULL
+    if (app == "inzight" &&
+        utils::packageVersion("iNZight") > numeric_version('4.1.4')) {
 
-		awin <- iNZight:::iNZAboutWidget$new(
-			title = "Loading iNZight ..."
-		)
+        awin <- iNZight:::iNZAboutWidget$new(
+            title = "Loading iNZight ..."
+        )
 
-	}
+    }
 
-	suppressWarnings(
-		switch(app,
-			'inzight' = iNZight(dispose_fun = q, save = "no"),
-			'vit' = iNZightVIT(dispose = TRUE)
-		)
-	)
+    suppressWarnings(
+        switch(app,
+            'inzight' = iNZight(dispose_fun = q, save = "no"),
+            'vit' = iNZightVIT(dispose = TRUE)
+        )
+    )
 
-	if (!is.null(awin)) dispose(awin$win)
+    if (!is.null(awin)) dispose(awin$win)
 }
 
 do_update <- function() {
-	# update the updater if any updates are available
-	cat("* Checking if the updater needs updating ...\n")
-	current <- utils::packageVersion('iNZightUpdate')
-	ap <- utils::available.packages(repos = options()$repos[1])
-	if ("iNZightUpdate" %in% rownames(ap)) {
-		latest <- ap['iNZightUpdate', 'Version']
-		if (current < numeric_version(latest))
-			utils::install.packages('iNZightUpdate')
-	}
+    # update the updater if any updates are available
+    cat("* Checking if the updater needs updating ...\n")
+    current <- utils::packageVersion('iNZightUpdate')
+    ap <- utils::available.packages(repos = options()$repos[1])
+    if ("iNZightUpdate" %in% rownames(ap)) {
+        latest <- ap['iNZightUpdate', 'Version']
+        if (current < numeric_version(latest))
+            utils::install.packages('iNZightUpdate')
+    }
 
-	# then run the updater
-	iNZightUpdate::update("windows")
+    # then run the updater
+    iNZightUpdate::update("windows")
 }
