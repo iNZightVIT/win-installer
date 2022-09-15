37480dd5d6f5fe1184fb76a73757d98cf19267bf
Tom Elliott <tomelliottnz@gmail.com>
2022-09-15 13:19:09 +1200

    add inzight option

--- launcher.R
+++ launcher.R
@@ -1,9 +1,9 @@
 # this file should always be called from a subdirectory
-setwd('..')
+setwd("..")
 
 
 options(
-    help_type = 'html',
+    help_type = "html",
     inzight.disable.bootstraps = TRUE,
     inzight.lock.packages = TRUE,
     inzight.default.dev.features = TRUE,
@@ -11,12 +11,12 @@ options(
 )
 
 Sys.setenv(
-    'R_HOME' = file.path(getwd(), 'R'),
-    'R_USER_CONFIG_DIR' = file.path(getwd(), ".config"),
-    'R_USER_CACHE_DIR' = file.path(getwd(), ".cache"),
-    'R_USER_DATA_DIR' = file.path(getwd(), "data"),
-    'INZIGHT_MODULES_DIR' = file.path(getwd(), "modules"),
-    'R_CACHE_ROOTPATH' = file.path(getwd(), ".cache")
+    "R_HOME" = file.path(getwd(), "R"),
+    "R_USER_CONFIG_DIR" = file.path(getwd(), ".config"),
+    "R_USER_CACHE_DIR" = file.path(getwd(), ".cache"),
+    "R_USER_DATA_DIR" = file.path(getwd(), "data"),
+    "INZIGHT_MODULES_DIR" = file.path(getwd(), "modules"),
+    "R_CACHE_ROOTPATH" = file.path(getwd(), ".cache")
 )
 
 #### Notes
@@ -31,7 +31,7 @@ create_dir(tools::R_user_dir("iNZight", "cache"))
 create_dir(tools::R_user_dir("iNZight", "data"))
 
 # set library path
-.libPaths(file.path(getwd(), 'library'))
+.libPaths(file.path(getwd(), "library"))
 
 ## ------------------------------------------------------------------------themeing
 guinz_palette <- list(
@@ -73,13 +73,15 @@ options(
     ),
     inzight.default.ggtheme.name = "GUiNZ",
     inzight.default.palette.cat.name = "GUiNZ",
-    ggplot2.discrete.fill = cat_pal
+    ggplot2.discrete.fill = cat_pal,
+    # if users try plotting a combination of cat/num vars, the numeric ones will be removed from the plot
+    inzight.auto.remove.noncatvars = TRUE
 )
 
-start_app <- function(app = c('inzight', 'vit', 'update')) {
-    app = match.arg(app)
+start_app <- function(app = c("inzight", "vit", "update")) {
+    app <- match.arg(app)
 
-    if (app == 'update') {
+    if (app == "update") {
         do_update()
         return()
     }
@@ -89,25 +91,32 @@ start_app <- function(app = c('inzight', 'vit', 'update')) {
     grid::grid.newpage()
     # Will try to draw a raster if possible, otherwise an array of pixels
 
-    try({
-        suppressWarnings({
-            splashImg <- png::readPNG(
-                file.path(
-                    getwd(),
-                    "inst",
-                    "inzight-banner_guinz.png"
-                ),
-                exists("rasterImage")
-            )
-        })
-        grid::grid.raster(splashImg)
-    }, silent = TRUE)
+    try(
+        {
+            suppressWarnings({
+                splashImg <- png::readPNG(
+                    file.path(
+                        getwd(),
+                        "inst",
+                        "inzight-banner_guinz.png"
+                    ),
+                    exists("rasterImage")
+                )
+            })
+            grid::grid.raster(splashImg)
+        },
+        silent = TRUE
+    )
 
     message("(Dept of Statistics, Uni. of Auckland)")
     message("")
     message(
-        sprintf("Please wait while %s loads...",
-            switch(app, 'inzight' = 'iNZight', 'vit' = 'VIT')
+        sprintf(
+            "Please wait while %s loads...",
+            switch(app,
+                "inzight" = "iNZight",
+                "vit" = "VIT"
+            )
         )
     )
 
@@ -115,8 +124,8 @@ start_app <- function(app = c('inzight', 'vit', 'update')) {
     suppressMessages(
         suppressWarnings({
             switch(app,
-                  'inzight' = library(iNZight),
-                  'vit' = library(vit)
+                "inzight" = library(iNZight),
+                "vit" = library(vit)
             )
         })
     )
@@ -127,18 +136,16 @@ start_app <- function(app = c('inzight', 'vit', 'update')) {
 
     awin <- NULL
     if (app == "inzight" &&
-        utils::packageVersion("iNZight") > numeric_version('4.1.4')) {
-
+        utils::packageVersion("iNZight") > numeric_version("4.1.4")) {
         awin <- iNZight:::iNZAboutWidget$new(
             title = "Loading iNZight ..."
         )
-
     }
 
     suppressWarnings(
         switch(app,
-            'inzight' = iNZight(dispose_fun = q, save = "no"),
-            'vit' = iNZightVIT(dispose = TRUE)
+            "inzight" = iNZight(dispose_fun = q, save = "no"),
+            "vit" = iNZightVIT(dispose = TRUE)
         )
     )
 
@@ -148,12 +155,13 @@ start_app <- function(app = c('inzight', 'vit', 'update')) {
 do_update <- function() {
     # update the updater if any updates are available
     cat("* Checking if the updater needs updating ...\n")
-    current <- utils::packageVersion('iNZightUpdate')
+    current <- utils::packageVersion("iNZightUpdate")
     ap <- utils::available.packages(repos = options()$repos[1])
     if ("iNZightUpdate" %in% rownames(ap)) {
-        latest <- ap['iNZightUpdate', 'Version']
-        if (current < numeric_version(latest))
-            utils::install.packages('iNZightUpdate')
+        latest <- ap["iNZightUpdate", "Version"]
+        if (current < numeric_version(latest)) {
+            utils::install.packages("iNZightUpdate")
+        }
     }
 
     # then run the updater
