e4585626b3abbb4cf568c7abb52cc7d3d31afb98
Tom Elliott <tomelliottnz@gmail.com>
2024-04-08 13:26:51 +1200

    error check

--- install_inzight.R
+++ install_inzight.R
@@ -6,15 +6,15 @@ options(
 )
 
 inz_pkgs <- c(
-  'iNZight',
-  'iNZightPlots',
-  'iNZightMR',
-  'iNZightTS',
-  'iNZightTools',
-  'iNZightRegression',
-  'vit',
-  'FutureLearnData',
-  'iNZightUpdate'
+  "iNZight",
+  "iNZightPlots",
+  "iNZightMR",
+  "iNZightTS",
+  "iNZightTools",
+  "iNZightRegression",
+  "vit",
+  "FutureLearnData",
+  "iNZightUpdate"
 )
 
 # install iNZight packages (except iNZightModules)
@@ -24,13 +24,17 @@ install.packages(
   type = "binary"
 )
 
+if (!requireNamespace("iNZight")) {
+  stop("iNZight not installed")
+}
+
 # install iNZightModules, and manually some of the dependencies
 # (basically, ensuring iNZightMaps and sf aren't installed)
 install.packages(
   c(
-    'iNZightModules',
-    'rgl',
-    'mgcv'
+    "iNZightModules",
+    "rgl",
+    "mgcv"
   ),
   type = "binary"
 )
